7814fb6ebc63a790b81978a74ef47a57e7e658e7
Ismael Bueno <ismaelwno@gmail.com>
2023-05-25 21:16:13 +0200

    Added necessary files to compile/link successfuly

--- premake5.lua
+++ premake5.lua
@@ -16,34 +16,41 @@ files
    "src/input.c",
    "src/monitor.c",
    "src/vulkan.c",
-   "src/window.c"
+   "src/window.c",
+   "src/internal.h",
+   "src/platform.h",
+   "src/mappings.h",
+   "src/platform.c",
+   "src/egl_context.c",
+   "src/osmesa_context.c",
+   "src/null_platform.h",
+   "src/null_joystick.h",
+   "src/null_init.c",
+   "src/null_monitor.c",
+   "src/null_window.c",
+   "src/null_joystick.c"
 }
-filter "system:macos"
+
+filter "system:macosx"
    pic "On"
 
    files
    {
-      "src/x11_init.c",
-      "src/x11_monitor.c",
-      "src/x11_window.c",
-      "src/xkb_unicode.c",
-      "src/posix_time.c",
+      "src/cocoa_platform.h",
+      "src/cocoa_joystick.h",
+      "src/cocoa_time.h",
+      "src/cocoa_time.c",
+      "src/posix_thread.h",
+      "src/posix_module.c",
       "src/posix_thread.c",
-      "src/glx_context.c",
-      "src/egl_context.c",
-      "src/osmesa_context.c",
-      "src/linux_joystick.c"
+      "src/cocoa_init.m",
+      "src/cocoa_joystick.m",
+      "src/cocoa_monitor.m",
+      "src/cocoa_window.m",
+      "src/nsgl_context.m"
    }
 
-   defines "_GLFW_X11"
-
-   buildoptions {
-      "-Wall",
-      "-Wextra",
-      "-Wpedantic",
-      "-Wconversion",
-      "-Wno-unused-parameter"
-   }
+   defines "_GLFW_COCOA"
 
 filter "system:windows"
 
@@ -52,12 +59,13 @@ filter "system:windows"
       "src/win32_init.c",
       "src/win32_joystick.c",
       "src/win32_monitor.c",
+      "src/win32_time.h",
+      "src/win32_thread.h",
       "src/win32_time.c",
       "src/win32_thread.c",
+      "src/win32_module.c",
       "src/win32_window.c",
       "src/wgl_context.c",
-      "src/egl_context.c",
-      "src/osmesa_context.c"
    }
 
    defines 
